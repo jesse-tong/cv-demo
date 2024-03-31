cae2d28defb80654a782548d37e8f21690ba6a1d
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2024-03-31 19:36:39 +0700

    Change Vite config so that the browser would not block github.io

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://jesse-tong.github.io/cv-demo/",
   build: {
     outDir: "build"
   },
