f9d218933c5d6133cede6153c44b7e81f9d1d64d
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2024-03-31 19:49:02 +0700

    Change Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./cv-demo/",
+  base: "https://jesse-tong.github.io/cv-demo",
   build: {
     outDir: "build"
   }
