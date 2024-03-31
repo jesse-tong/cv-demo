48a25259916bf80be52292535c1ac1819d0344da
Glitch (glitch-hello-react) <none>
2024-03-31 02:54:00 +0000

    🚎🚆 Checkpoint ./src/components/cv_header.jsx:65055676/300

--- src/components/cv_header.jsx
+++ src/components/cv_header.jsx
@@ -8,5 +8,17 @@ function CVHeader({image, name, role, personal_info, contact_list}){
   
   const gender = personal_info.get('gender') || 'Other';
   const birth_date = personal_info.get('birth_date') || '';
-  const birth_place = personal_info.get('birth_place')
+  const birth_place = personal_info.get('birth_place');
+  
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="mx-4 mt-4 mb-3 row" >
+          <div className="col-9 ms-0 my-0 me-3">
+            <h1 style='fontFamily: '></h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
