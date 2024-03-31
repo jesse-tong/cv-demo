279e7b0e3453d0ba8c471fc4dce3aa2246c77c1c
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2024-03-31 19:45:51 +0700

    Change Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jesse-tong.github.io/cv-demo/",
+  base: "./cv-demo/",
   build: {
     outDir: "build"
-  },
-  server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
   }
 });
