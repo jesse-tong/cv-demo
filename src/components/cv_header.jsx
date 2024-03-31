1be4169a212e0c393e0fad16c11ea34eeaac9e05
Glitch (glitch-hello-react) <none>
2024-03-31 02:42:56 +0000

    🙆🏯 Checkpoint ./src/components/cv_header.jsx:65055676/452

--- src/components/cv_header.jsx
+++ src/components/cv_header.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-function CVHeader({image, name, role, contact_list}){
+function CVHeader({image, name, role, personal_info, contact_list}){
+  const phone_number = contact_list['phone_number'];
+  const email = contact_list['email'];
+  const linkedin = contact_list.get('linkedin');
+  const git = contact_list.get('git'); //Remote git repos, such as Github or Gitlab or Codeberg
   
+  const gender = personal_info.get('gender') || 'Other';
+  const birth_date = personal_info.get('birth_date') || '';
+  const birth_place = personal_info.get('birth_place')
 }
